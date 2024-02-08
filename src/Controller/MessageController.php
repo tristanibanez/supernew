844926b4afe786ae823863adba5633a8de1d253c
Tristan Ibañez <tristanuab@gmail.com>
2024-02-08 10:07:35 +0100

    "new message" page with form

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class MessageController extends AbstractController
 {
@@ -13,6 +19,37 @@ class MessageController extends AbstractController
     {
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+        ]);
+    }
+
+
+    #[Route('/new', name: 'newform')]
+    public function new(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        // creates a message object
+        $message = new Message();
+
+        $form = $this->createFormBuilder($message)
+            ->add('name', TextType::class)
+            ->add('message', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Save message'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $message_yo = $form->getData();
+
+
+            // tell Doctrine you want to (eventually) save the Message (no queries yet)
+            $entityManager->persist($message_yo);
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();            
+            return $this->redirectToRoute('newform');
+        }
+        return $this->render('message/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
