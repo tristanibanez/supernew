6a4883c0afff206df39fc4382991ea5ed674f462
Tristan Ibañez <tristanuab@gmail.com>
2024-02-09 08:18:28 +0100

    BUGFIX Show all messages + fixes submiting messages

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Message;
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +16,34 @@ use Symfony\Component\HttpFoundation\Request;
 class MessageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(MessageRepository $messageRepository, EntityManagerInterface $entityManager, Request $request): Response
     {
+        // creates a message object
+        $message = new Message();
+
+        $form = $this->createFormBuilder($message)
+            ->add('name', TextType::class)
+            ->add('message', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Save message'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $message_yo = $form->getData();
+
+
+            // tell Doctrine you want to (eventually) save the Message (no queries yet)
+            $entityManager->persist($message_yo);
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();            
+            return $this->redirectToRoute('newform');
+        }
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+            'messages' => $messageRepository->findAll(),
+            'form' => $form,
         ]);
     }
 
